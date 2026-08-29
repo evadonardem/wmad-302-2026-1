356630ca26145fcc231da911ff5faf1c3eba56d6
Grace David <gracemdavid7@gmail.com>
2026-08-29 16:48:23 +0800

    Complete Module 4

--- 01_JS_Fundamentals_Part1_David_Grace/topics/04_advanced_functions.js
+++ 01_JS_Fundamentals_Part1_David_Grace/topics/04_advanced_functions.js
@@ -2,10 +2,29 @@ import console from 'node:console';
 
 export function memoize(fn) {
   // TODO: Cache evaluation results in a local object closure
+  const cache = {};
+  return function (...args) {
+    const key = JSON.stringify(args);
+    
+    if (cache.hasOwnProperty(key)) {
+      return cache[key];
+    }
+    const result = fn(...args);
+    cache[key] = result;
+    return result;
+  };
 }
 
 export function createJeepneyFareCalculator(baseFare = 13, discountRate = 0.20) {
   // TODO: Return closure (distanceKm, isStudentOrSenior) calculating fare
+  return function (distanceKm, isStudentOrSenior) {
+    const extraDistance = Math.max(0, distanceKm - 4);
+    let total = baseFare + (extraDistance * 1.75);
+    if (isStudentOrSenior) {
+      total = total * (1 - discountRate);
+    }
+    return Number(total.toFixed(2));
+  };
 }
 
 export function runAdvancedFunctionsTests() {
