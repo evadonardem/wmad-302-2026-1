61cbe0c5f1f97b105071954cc7c19577ff433a00
Karyll Kila-an <your_email@gmail.com>
2026-09-05 21:58:13 +0800

    01_JS_Fundaamentals_Part1--module1/task1 and task2 accomplished

--- 01_JS_Fundamentals_Part1_Kila-an_Karyll/topics/01_fundamentals.js
+++ 01_JS_Fundamentals_Part1_Kila-an_Karyll/topics/01_fundamentals.js
@@ -1,11 +1,29 @@
 import console from 'node:console';
 
 export function evaluateAyudaEligibility(citizen) {
-  // TODO: Task 1 - Evaluate Ayuda Eligibility using ?? and logical operators
+
+const dependents = citizen.dependentCount ?? 0;
+
+  return citizen.isSeniorPWD ||
+         (citizen.isLowIncome && dependents >= 3);
+  
 }
 
 export function computeJollibeeBill(rawPrice, isSeniorOrPWD) {
-  // TODO: Task 2 - Compute bill returning rounded Number (e.g., Number(total.toFixed(2)))
+ 
+   if (typeof rawPrice !== 'number' || isNaN(rawPrice) || rawPrice <= 0) {
+    return 0;
+  }
+
+  let total;
+
+  if (isSeniorOrPWD) {
+    total = rawPrice * 0.80;
+  } else {
+    total = rawPrice * 1.12;
+  }
+
+  return Number(total.toFixed(2));
 }
 
 export function runFundamentalsTests() {
