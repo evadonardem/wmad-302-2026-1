167ba14d886a04aca85b6f784b54d5223736ee8e
May-anne Asiab <mayasiab6@gmail.com>
2026-09-26 17:24:19 +0800

    04_React_My_First_App_Asiab_May-anne

--- 04_React_My_First_App_Asiab_May-anne/src/App.jsx
+++ 04_React_My_First_App_Asiab_May-anne/src/App.jsx
@@ -8,20 +8,26 @@ import GeneralSettings from './components/GeneralSettings';
 // TODO 13 [Dynamic Themes]: Complete the theme creation arrow function.
 // It should accept a 'mode' string parameter ('light' or 'dark') and generate an MUI theme object configuration mapping that mode.
 const theme = (mode = 'light') => createTheme({
-  // [Your code here]
+  palette: {
+    mode,
+    primary: {
+      main: '#B5424A'
+    }
+  },
 });
 
 function App() {
   // TODO 14 [State Management]: Initialize a boolean React state hook variable named 'isDarkMode' defaulting to false.
-  // [Your code here]
+  const [isDarkMode, setisDarkMode] = useState(false)
 
   // TODO 15 [Data Actions Mapping]: Populate the 'actions' configuration array below.
   // Ensure the first action toggle object displays a <LightMode /> icon if 'isDarkMode' is true, or a <DarkMode /> icon if false.
   // The 'onClick' function must invert the current boolean state value of 'isDarkMode' upon execution.
   const actions = [
     {
-      name: 'Light Mode' , // isDarkMode ? 'Light Mode' : 'Dark Mode',
-      // [Your code here: Add dynamic icon and state-toggling onClick function]
+      icon: isDarkMode ? <LightMode/> : <DarkMode/>,
+      name: isDarkMode ? 'Light Mode' : 'Dark Mode',
+      onClick: () => setisDarkMode(currentMode => !currentMode)
     },
     { icon: <Palette />, name: 'Theme' },
     { icon: <Print />, name: 'Print' },
@@ -32,19 +38,20 @@ function App() {
     // TODO 16 [Theme Binding Layout]: Wrap the children inside a dynamic ThemeProvider passing the calculated theme mode.
     // Configure the layout context matching: mode should resolve to 'dark' if 'isDarkMode' is true, otherwise 'light'.
     <>
-      {/* [Your ThemeProvider wrapper structure here] */}
-      <CssBaseline />
-      <Container sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        minHeight: '95vh',
-        width: '100vw',
-      }}>
-        <QuoteOfTheDay />
-        <GeneralSettings actions={actions} />
-      </Container>
+      <ThemeProvider theme={theme(isDarkMode ? `dark` : `light`)}>
+        <CssBaseline />
+        <Container sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          minHeight: '95vh',
+          width: '100vw',
+        }}>
+          <QuoteOfTheDay />
+          <GeneralSettings actions={actions} />
+        </Container>
+      </ThemeProvider>    
     </>
   )
 }
